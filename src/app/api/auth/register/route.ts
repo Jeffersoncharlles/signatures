62989055ba0265c1d8eb4f68f78ac8966068a86d
Jefferson Charlles <contato@jefferdeveloper.com>
2025-04-03 10:55:01 -0300

    fix: route register test

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -14,7 +14,7 @@ const registerSchema = z.object({
     .min(3, "Password must have than 8 characters"),
 });
 
-export const POST = async (request: Request) => {
+export async function POST(request: Request) {
   try {
     const { email, name, password } = registerSchema.parse(
       await request.json()
@@ -46,4 +46,4 @@ export const POST = async (request: Request) => {
   }
 
   return NextResponse.json({ message: "success" });
-};
+}
