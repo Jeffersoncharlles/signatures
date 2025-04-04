4ca825de4ea8d0a582c44d2fca288899287e5ab6
Jefferson Charlles <contato@jefferdeveloper.com>
2025-04-04 06:37:19 -0300

    fix: deploy

--- src/app/(private)/(docs)/_actions/fetchdocuments.ts
+++ src/app/(private)/(docs)/_actions/fetchdocuments.ts
@@ -4,7 +4,7 @@ import { prisma } from "@/services/prisma";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 
-export const dynamic = "force-dynamic"; // Garante que será executado no servidor
+// export const dynamic = "force-dynamic"; // Garante que será executado no servidor
 
 export async function fetchDocuments() {
   try {
