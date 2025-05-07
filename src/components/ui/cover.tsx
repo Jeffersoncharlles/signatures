e3f5b8529780cb91f934eb7fca19f58e168ba276
Jefferson Charlles <contato@jefferdeveloper.com>
2025-05-07 19:02:16 -0300

    refacture: ui implemation fix

--- src/components/ui/cover.tsx
+++ src/components/ui/cover.tsx
@@ -31,7 +31,7 @@ export const Cover = ({
       );
       setBeamPositions(positions);
     }
-  }, [ref.current]);
+  }, [ref]);
 
   return (
     <div
@@ -212,6 +212,7 @@ export const Beam = ({
 
 export const CircleIcon = ({
   className,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   delay,
 }: {
   className?: string;
